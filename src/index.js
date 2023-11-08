abfcba42960e0ac3968762ee7edc5959df012075
rafinhahdc19 <124531634+rafinhahdc19@users.noreply.github.com>
2023-11-07 23:57:45 -0300

    velocidade aprimorada

--- src/index.js
+++ src/index.js
@@ -1,28 +1,67 @@
 const CoinKey = require("coinkey")
 const axios = require("axios")
-const fs = require("fs")
+const fs = require("fs").promises
+const numWallets = 30
 //bpp = balance, pvk(privateKey), pbk(publicKey)
-function salvarBPP(bpp){
-    fs.writeFile("./key.txt", JSON.stringify(bpp)  + '\n', (err) => {
-        if(err) throw err
+async function salvarBPP(bpp){
+    const stringFromBpp = JSON.stringify(bpp)
+    await fs.writeFile("./key.txt", stringFromBpp  + '\n', (err) => {
+        if(err) console.log(err)
         console.log("A bpp foi salva")
     })
 }
 
-async function getNewWallet(){
-    const wallet = new CoinKey.createRandom()
-    const response = await axios.get("https://blockchain.info/balance?active="+wallet.publicAddress)
-    return {"balance":response.data[wallet.publicAddress].final_balance,"pvk":wallet.privateKey.toString("hex"),"pbk":wallet.publicAddress}
+async function getNewWallet(numeroDeCarteiras){
+    const carteiras = [];
+    const pvk_pbk = []; 
+    
+    for (let i = 1; i <= numeroDeCarteiras; i++) {
+        const wallet = new CoinKey.createRandom();
+        const pvk = wallet.privateKey.toString("hex");
+        const pbk = wallet.publicAddress;
+        
+        carteiras.push(pbk);
+        pvk_pbk.push({ pvk, pbk });
+    }
+    
+    const enderecosParaConsulta = carteiras.join(",");
+    
+    const response = await axios.get("https://blockchain.info/balance?active=" + enderecosParaConsulta);
+    const dados = response.data;
+    
+    const walletsAndBalance = [];
+    
+    for (const endereco in dados) {
+        const balance = dados[endereco].final_balance;
+        const pvk_pbk_pair = pvk_pbk.find(pair => pair.pbk === endereco);
+        
+        if (pvk_pbk_pair) {
+            const { pvk, pbk } = pvk_pbk_pair;
+            walletsAndBalance.push({ balance : balance, pvk, pbk });
+        }
+    }
+    
+    console.log(walletsAndBalance);
+    return walletsAndBalance;
 }
-async function main(){
-    const wallet = await getNewWallet()
-    console.log(wallet)
-    if (wallet.balance > 0) {
-        console.log("você encontrou btc, privateKey:"+wallet.pvk)
-        await salvarBPP(wallet)
-        process.exit(0)
+async function main(numeroDeCarteiras){
+    let encontrado = false; 
+    const wallets = await getNewWallet(numeroDeCarteiras)
+    //console.log(wallet)
+    for (const wallet of wallets) {
+        console.log(wallet.balance)
+        if (wallet.balance > 0) {
+            encontrado = true
+            console.log("Você encontrou BTC, privateKey:", wallet.pvk);
+            await salvarBPP(wallet);
+            process.exit()
+        }
+    }
+    if(encontrado == false){
+        main(numWallets)
     }else{
-        main()
+        process.exit(0)
     }
 }
-main()
+
+main(numWallets)
